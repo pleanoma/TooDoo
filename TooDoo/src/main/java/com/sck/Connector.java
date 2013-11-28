a066f3da1de51b1b1eabece429789fd941c10aa2
unknown <Noppon@TECH21.silkspan.local>
2013-11-28 16:13:21 +0700

    3

--- TooDoo/src/main/java/com/sck/Connector.java
+++ TooDoo/src/main/java/com/sck/Connector.java
@@ -13,41 +13,62 @@ import javax.sql.DataSource;
 
 public class Connector {
 
-	public static ResultSet executeQuery(String strSql) throws SQLException {
+	public static TooDooList executeQueryList(String strName) throws SQLException {
 
 		Connection con = null;
 		ResultSet rs;
 		Statement pstmt;
+		TooDooList list = new TooDooList();
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://192.168.0.90/voip?user=webapp&password=password");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/TooDoo?user=root&password=1234");
 			con.setAutoCommit(false);
-			pstmt = con.prepareStatement(strSql);
+			//pstmt = con.prepareStatement("select * from chat order by id");
+			pstmt = con.prepareStatement("select * from TooDooList");
 
-			rs = pstmt.executeQuery(strSql);
+			rs = pstmt.executeQuery("select * from TooDooList");
 			ResultSetMetaData md = rs.getMetaData();
 
 	
 			while (rs.next()) {
-				Category cat = new Category();
-				TooDooList list = new TooDooList();
-				
-				cat.setUserID(String.valueOf( rs.getInt(1)));
-				cat.setCatName(rs.getString(2));
+				System.out.println("a");
+				list.setId(rs.getInt(1));
 				list.setSubject(rs.getString(2));
-				list.setDue_date(Date.valueOf(rs.getString(2)));
-				list.setUserID(rs.getString(2));
-				list.set;(rs.getString(2));
-				list.setDue_date(rs.getString(2));
-				list.setDue_date(rs.getString(2));
-				list.setDue_date(rs.getString(2));
-				list.setDue_date(rs.getString(2));
-				list.setDue_date(rs.getString(2));
-				
-				
-				
-				String message = rs.getString(5);
-				System.out.println(id);
-				System.out.println(message);
+				list.setDue_date(Date.valueOf(rs.getString(3)));
+				list.setUserID(rs.getString(4));
+				list.setCatID(rs.getInt(5));
+				list.setPriority(rs.getString(6));
+				list.setCreate_Date(Date.valueOf(rs.getString(7)));
+				System.out.println("b");
+				System.out.println(list);
+			}
+		} finally {
+			if (con != null)
+				con.close();
+		}
+		return list;
+	}
+	
+	public static Category executeQueryCat(String strName) throws SQLException {
+
+		Connection con = null;
+		ResultSet rs;
+		Statement pstmt;
+		Category cat = new Category();
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/TooDoo?user=root&password=1234");
+			con.setAutoCommit(false);
+			//pstmt = con.prepareStatement("select UserID,CatName,CatID from Category");
+			pstmt = con.prepareStatement("select * from Category");
+
+			rs = pstmt.executeQuery("select * from Category");
+			ResultSetMetaData md = rs.getMetaData();
+	
+			while (rs.next()) {
+				System.out.println("a");
+				cat.setUserID(rs.getString(1));
+				cat.setCatName(rs.getString(2));
+				cat.setCatID(rs.getInt(3));
+				System.out.println("b");
 			}
 			
 			//con.commit();
@@ -57,9 +78,8 @@ public class Connector {
 			if (con != null)
 				con.close();
 		}
-		return rs;
+		return cat;
 	}
-	
 	
 	public static ResultSet executeNonQuery(String strSql) throws SQLException {
 
@@ -91,19 +111,21 @@ public class Connector {
 		}
 		return rs;
 	}
-
+/*
 	public static void main(String[] args) throws SQLException {
 		//Connector connector = new Connector();
 		System.out.println("ssssssssssss");
-		ResultSet rs = executeQuery("select * from chat order by id");
+		TooDooList ppL = executeQueryList("adad");
+		//Category ppC = executeQueryCat("adad");
 		System.out.println("dddd");
-		/*
+		
 		while (rs.next()) {
 			//int id = rs.getInt(1);
 			String message = rs.getString(5);
 			//System.out.println(id);
 			System.out.println(message);
-		}*/
+		}
 
 	}
+*/
 }
